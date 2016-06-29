bd2844150918a99b990f6120ed3eb7854e6edcc1
sinish <sinish@amt.in>
2016-06-29 14:34:05 +0530

    add prefix

--- lower/lower-case.js
+++ lower/lower-case.js
@@ -1,11 +1,12 @@
 module.exports = function(RED) {
     function LowerCaseNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
+     RED.nodes.createNode(this,config);
+     this.prefix = config.prefix;
+     var node = this;
+     this.on('input', function(msg) {
+         msg.payload = node.prefix + msg.payload.toLowerCase();
+         node.send(msg);
+     });
+ }
     RED.nodes.registerType("lower-case",LowerCaseNode);
 }
